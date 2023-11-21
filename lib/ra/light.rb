94565f9534d7b7585ee27509f4af4859c88a8ce4
Kevin Sylvestre <kevin@ksylvest.com>
2023-11-20 16:34:54 -0800

    Define a light for rendering

---
+++ lib/ra/light.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Ra
+  # A light has a position within a scene and an intensity it is rendered
+  # with. For example:
+  #
+  #   light = Ra::Light.new(
+  #     intensity: Ra::Color.uniform(0.8),
+  #     position: Vector[0, 0, 0, Ra::Tuple::POINT],
+  #   )
+  class Light
+    attr_accessor :intensity, :position
+
+    # @param intensity [Ra::Color]
+    # @param position [Ra::Tuple]
+    def initialize(intensity:, position:)
+      @intensity = intensity
+      @position = position
+    end
+  end
+end
