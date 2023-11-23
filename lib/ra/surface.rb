ebb794c4fe6893a85a4c46e627c7965056eee46f
Kevin Sylvestre <kevin@ksylvest.com>
2023-11-23 12:03:32 -0800

    Track a reflectv on surface

--- lib/ra/surface.rb
+++ lib/ra/surface.rb
@@ -3,15 +3,16 @@
 module Ra
   # A surface contains everything needed to apply lighting.
   class Surface
-    attr_accessor :eyev, :normalv, :shape, :point
+    attr_accessor :eyev, :normalv, :reflectv, :shape, :point
 
     # @param eyev [Vector]
     # @param normalv [Vector]
     # @param shape [Ra::Shape]
     # @param point [Vector]
-    def initialize(eyev:, normalv:, shape:, point:)
+    def initialize(eyev:, normalv:, reflectv:, shape:, point:)
       @eyev = eyev
       @normalv = normalv.dot(eyev).negative? ? -normalv : +normalv
+      @reflectv = reflectv
       @shape = shape
       @point = point
     end
